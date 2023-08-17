c969370313fb87abff48d14df965f7df0c62cf94
Retorin <Joakim.friksen.reigstad@gmail.com>
2023-08-17 18:19:01 +0200

    update

--- app.js
+++ app.js
@@ -13,15 +13,23 @@ optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}d
 // Creating boards
 const width = 10
 
-function createBoard(color) {
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement('div')
     gameBoardContainer.classList.add('game-board')
     gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
+
+    for (let i = 0; i < width*width; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
 
     gamesBoardContainer.appendChild(gameBoardContainer)
 }
 
 // Create and append boards
-createBoard('yellow');
-createBoard('purple')
+createBoard('yellow', 'player');
+createBoard('purple', 'computer');
 flipButton.addEventListener('click', flip)
